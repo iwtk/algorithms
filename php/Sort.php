430004c3928861de9497310efdd5f23b41ba5b85
Ray Iwatsuki <weatherbreak@wine.ocn.ne.jp>
2017-05-25 03:40:06 -0700

    Update Sort.php

--- php/Sort.php
+++ php/Sort.php
@@ -6,6 +6,14 @@ class Sort {
                 if ($arr[$j] > $arr[$j+1]) self::swap($arr, $j, $j+1);
     }
     
+    public static function insertionSort($arr) {
+        for ($i = 1; $i < count($arr); $i++)
+            for ($j = $i-1; $j >= 0; $j--) {
+                if ($arr[$j] < $arr[$j+1]) break;
+                self::swap($arr, $j, $j+1);
+            }
+    }
+    
     private function swap($arr, $a, $b) {
         $tmp = $arr[$a];
         $arr[$a] = $arr[$b];
@@ -14,7 +22,8 @@ class Sort {
 }
 
 $arr = array(8, 3, 1, 2, 7, 5, 6, 4);
-Sort::bubbleSort($arr);
+//Sort::bubbleSort($arr);
+Sort::insertionSort($arr);
 
 foreach ($arr as $val)
     echo $val;
