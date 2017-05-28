10f1da0cca02ce4518c585ba8c43a28bd6bcf975
Ray Iwatsuki <weatherbreak@wine.ocn.ne.jp>
2017-05-27 17:29:01 -0700

    Update Sort.java

--- java/Sort.java
+++ java/Sort.java
@@ -8,17 +8,17 @@ public class Sort {
     //quickSort(arr, 0, arr.length-1);
     mergeSort(arr, 0, arr.length-1);
     for (int v: arr)
-      System.out.println(v);
+      System.out.print(v);
   }
   
   public static void bubbleSort(int[] arr) {
-    for (int i = arr.length - 1; i >= 1; i--)
+    for (int i = arr.length-1; i >= 1; i--)
       for (int j = 0; j < i; j++)
         if (arr[j] > arr[j+1]) swap(arr, j, j+1);
   }
   
   public static void insertionSort(int[] arr) {
-    for (int i = 1; i < arr.length; i++)
+    for (int i = 1; i <= arr.length-1; i++)
       for (int j = i-1; j >= 0; j--) {
         if (arr[j] < arr[j+1]) break;
         swap(arr, j, j+1);
@@ -26,15 +26,15 @@ public class Sort {
   }
   
   public static void selectionSort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++)
-      for (int j = i+1; j < arr.length; j++)
+    for (int i = 0; i < arr.length-1; i++)
+      for (int j = i+1; j <= arr.length-1; j++)
         if (arr[i] > arr[j]) swap(arr, i, j);
   }
   
   public static void shellSort(int[] arr) {
     int[] gaps = {8, 4, 2, 1};
     for (int g : gaps)
-      for (int i = g; i < arr.length; i += g)
+      for (int i = g; i <= arr.length-1; i += g)
         for (int j = i; j >= g; j -= g) {
           if (arr[j-g] < arr[j]) break;
           swap(arr, j, j-g);
